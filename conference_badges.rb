b005d45379f4e68540a851e02d131f1b88cef990
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-16 01:46:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name) 
+ "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+ array.collect{|a| "Hello, my name is " + a + "."} 
+end 
+
+def assign_rooms(array)
+index = 0
+array.collect{|a| "Hello, #{a}! You'll be assigned to room #{index += 1}!"}
+end 
+
+def printer(array)
+  assign_rooms.each{|a| puts a}
+  
+end 
+
+ 
